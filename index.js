0e4c024ffb48458bc54eb36994649020ed620727
MA-Guodong <fgu6745@gmail.com>
2023-11-30 08:41:12 +0800

    解决跨域请求问题

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import singsRoutes from "./routes/signs.js";
 import avatarsRoutes from "./routes/avatar.js";
 import videosRoutes from "./routes/videos.js";
@@ -7,12 +8,19 @@ import { pool } from "./database/databasePool.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use("/api/signs", singsRoutes);
 app.use("/api/avatars", avatarsRoutes);
 app.use("/api/videos", videosRoutes);
 app.use("/api/charts", chartsRoutes);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 pool.getConnection((err, connection) => {
     if (err) {
@@ -23,6 +31,6 @@ pool.getConnection((err, connection) => {
     pool.releaseConnection(connection); // 释放连接，以便下次使用
 });
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
-})
+app.listen(3030, () => {
+    console.log("Server is listening on port 3030");
+});
